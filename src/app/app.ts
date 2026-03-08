4325da2d16997c07e2f9600fbb3911c423ab31cf
Mohamad Aiman Einihe <mohamad.einihe@atlasgroupone.com>
2026-03-08 09:18:41 +0300

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -56,7 +56,7 @@ export class App implements OnInit {
       alert('✅ تم نسخ الرمز السري بنجاح!\n\nسيتم الآن توجيهك لتحميل الاختصار.\nعندما يطلب منك الآيفون الرمز، فقط اضغط "لصق".');
 
       // توجيه المستخدم إلى رابط الاختصار الموحد
-      const icloudShortcutLink = 'https://www.icloud.com/shortcuts/afa59b2c3e2b4e3e9de04ef0ceac1e57';
+      const icloudShortcutLink = 'https://www.icloud.com/shortcuts/a3fdb18337ae433d97639af0c77cf264';
       window.open(icloudShortcutLink, '_blank');
 
     }).catch(err => {
